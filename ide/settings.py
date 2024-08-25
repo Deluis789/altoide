cdfc765debf51320ba5185f945a2cab39ff8a39e
luis diaz <luiscallizaya2934@gmail.com>
2024-08-25 15:01:35 -0300

    modulo3

--- ide/settings.py
+++ ide/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'ide.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "elAlto",
-        "USER": "postgres",
-        "PASSWORD": "admin",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
